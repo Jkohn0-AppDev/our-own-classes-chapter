24c814f994849a313153810596d57e834f5580a1
jkohn0 <jkohn0@chicagobooth.edu>
2022-01-22 22:17:22 +0000

    90% done with Class assignment

--- class_person.rb
+++ class_person.rb
@@ -7,5 +7,20 @@
 #  - An instance method, age, that calculates the number of years between today and birthdate
 
 class Person
-  
+  require("date")
+  attr_accessor :first_name
+  attr_accessor :last_name
+  attr_accessor :birthdate
+
+  def full_name
+    return self.first_name + " " + self.last_name
+  end
+
+  def age
+    this_year = Date.today.year
+    birth_year = Date.parse#{birthdate.year}
+    difference_in_years = this_year - birth_year
+    return difference_in_years.to_i
+  end
+
 end
